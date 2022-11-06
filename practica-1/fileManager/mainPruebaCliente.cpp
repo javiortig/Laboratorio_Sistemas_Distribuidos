e277f7d799e155615ac45875fe9468910420d5bd
javiortig <dev.javiog@gmail.com>
2022-11-06 15:14:33 +0000

    core filemanager hotfix

--- practica-1/fileManager/mainPruebaCliente.cpp
+++ practica-1/fileManager/mainPruebaCliente.cpp
@@ -6,24 +6,28 @@
 */
 int main(int argc,char** argv)
 {
+    std::cout << "Traza0\n";
     FileManagerClient* fmc = new FileManagerClient();
 
-    //Probamos a listar los directorios
-    fmc->listFiles();
-    usleep(1);
+    std::cout << "Traza1\n";
+    //Probamos a obtener los directorios
+    vector<string*>* vfiles = fmc->listFiles();
+    
 
+    std::cout << "Traza2\n";
     //leer archivo del servidor
-    char nombre_leer[] = "leer1.txt";
+    char nombre_leer[] = "prueba1.txt";
     char *buffer = nullptr;
     unsigned long bufferLen = -1;
     fmc->readFile(nombre_leer, buffer, bufferLen);
     std::cout << buffer << endl;
 
+    std::cout << "Traza3\n";
     //probamos a escribir datos en el directorio escribir.txt del server
     char* nombre_escribir = "escribir.txt";
     char *bufferB = "Escribo desde el cliente";
     fmc->writeFile(nombre_escribir, bufferB, strlen(buffer));
 
-
+    std::cout << "TrazaExit\n";
     return 0;
 }
